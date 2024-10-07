e51e144049cce74750da6f79269c2e8531d5f443
la229028 <la229028@student.helha.be>
2024-10-07 11:23:59 +0200

    Implémentation de la page Login + SCSS

---
+++ project_web_2024-2025/src/composants/LoginButton.tsx
@@ -0,0 +1,9 @@
+//date : 06/10/2024
+
+export default function LoginButton() {
+    return (
+        <div>
+            <button>Connexion</button>
+        </div>
+    )
+}
---
+++ project_web_2024-2025/src/composants/LoginPage.tsx
@@ -0,0 +1,21 @@
+//date : 06/10/2024
+
+import {useState} from "react";
+import LoginButton from "./LoginButton.tsx";
+import "../Ressources/Styles/StyleLogin.scss"
+
+export default function LoginPage() {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    return (
+        <form>
+            <label htmlFor="username">Nom d'utilisateur</label>
+            <input type="text" placeholder="Nom d'utilisateur" value={username} onChange={e => setUsername(e.target.value)} />
+            <label htmlFor="password">Mot de passe</label>
+            <input type="password" placeholder="Mot de passe" value={password} onChange={e => setPassword(e.target.value)} />
+            <LoginButton />
+        </form>
+    )
+}
